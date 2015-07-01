d7cc99472bb88c1969aacb4a69727e54c484ed23
Charles Vestur <charles.vestur@gmail.com>
2015-07-01 09:18:43 +0200

    Form to enter activity in database

--- client/form_database.js
+++ client/form_database.js
@@ -1,6 +1,39 @@
+AutoForm.hooks({    
+    submitActivity: {
+      before: {
+        insert: function(doc) {
+          doc.exactPrice = $('#exactPrice-radio').prop('checked');
+/*          if($("[name='image']").val() === '')
+         	 doc.image = $('#illustration').val();
+*/          doc.submitted = new Date();
+          this.result(doc);
+        }
+      } 
+    }     
+  });
+
+Template.formDatabase.onRendered(function(){
+$('.summernote').summernote({
+  toolbar: [
+    //[groupname, [button list]]
+     
+    ['style', ['bold', 'italic', 'underline', 'clear']],
+    ['font', ['strikethrough', 'superscript', 'subscript']],
+    ['fontsize', ['fontsize']],
+    ['color', ['color']],
+    ['para', ['ul', 'ol', 'paragraph']],
+    ['height', ['height']],
+  ]
+	});
+$(".list-group-item").css({'display': 'table-cell', 'padding':'0 0 0 20px', 'border': 'none'});
+});
+
 Template.formDatabase.helpers({
 	valueHelper: function() {
 		return $('#defaultValue').val();
+	},
+	test: function()  {
+		return Activities.find({});
 	}
 });
 
